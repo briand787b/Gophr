77cf647b5fd99c2033140c4c030999e8f977e983
briand787b <briand787b@gmail.com>
2017-04-06 21:10:23 -0400

    still initializing

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
-package Gophr
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+func main() {
+	log.Fatal(http.ListenAndServe(":3000", http.StripPrefix("/assets/",
+		http.FileServer(http.Dir("assets/")))))
+}
--- user.go
+++ user.go
@@ -1,1 +1,1 @@
-package Gophr
+package main
