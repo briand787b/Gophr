2ea7d9b8bb361832f28243e43b42e723f2047d95
briand787b <briand787b@gmail.com>
2017-04-11 21:41:05 -0400

    worked on templating

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"net/http"
-	"log"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		RenderTemplate(w, r, "index/home", nil)
+	})
+
 	mux.Handle("/assets/", http.StripPrefix("/assets/",
 		http.FileServer(http.Dir("assets/"))))
 
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	http.ListenAndServe(":3000", mux)
 }
