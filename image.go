87204b29504161334d86b2e3bf9548a03c858303
Brian D'Ostilio <briand787b@gmail.com>
2017-04-23 14:17:01 -0400

    added ability to upload images from url and local file system

--- image.go
+++ image.go
@@ -1,6 +1,14 @@
 package main
 
-import "time"
+import (
+	"time"
+	"net/http"
+	"mime"
+	"path/filepath"
+	"os"
+	"io"
+	"mime/multipart"
+)
 
 type Image struct {
 	ID 		string
@@ -19,3 +27,92 @@ type ImageStore interface {
 	FindAllByUser(user *User, offset int) ([]Image, error)
 }
 
+const imageIDLength = 10
+
+// A map of accepted mime types and their file extension
+var mimeExtensions = map[string]string {
+	"image/png": 	".png",
+	"image/jpeg":	".jpg",
+	"image/gif": 	".gif",
+}
+
+func NewImage(user *User) *Image {
+	return &Image{
+		ID:		GenerateID("img", imageIDLength),
+		UserID:		user.ID,
+		CreatedAt: 	time.Now(),
+	}
+}
+
+func (image *Image) CreateFromURL(imageURL string) error {
+	// Get the response from the URL
+	response, err := http.Get(imageURL)
+	if err != nil {
+		return err
+	}
+
+	// Make sure we have a valid response
+	if response.StatusCode != http.StatusOK {
+		return errImageURLInvalid
+	}
+
+	defer response.Body.Close()
+
+	// Ascertain the type of the file we downloaded
+	mimeType, _, err := mime.ParseMediaType(response.Header.Get("Content-Type"))
+	if err != nil {
+		return errInvalidImageType
+	}
+
+	// Get an extension for the file
+	ext, valid := mimeExtensions[mimeType]
+	if !valid {
+		return errInvalidImageType
+	}
+
+	// Get a name from the URL
+	image.Name = filepath.Base(imageURL)
+	image.Location = image.ID + ext
+
+	// Open a file at target location
+	savedFile, err := os.Create("./data/images/" + image.Location)
+	if err != nil {
+		return err
+	}
+	defer savedFile.Close()
+
+	// Copy the entire response to teh output file
+	size, err := io.Copy(savedFile, response.Body)
+	if err != nil {
+		return err
+	}
+	// The returned value from io.Copy is the number of bytes copied
+	image.Size = size
+
+	// Save our image to the store
+	return globalImageStore.Save(image)
+}
+
+func (image *Image) CreateFromFile(file multipart.File, headers *multipart.FileHeader) error {
+	// Move our file to an appropriate place, with an appropriate name
+	image.Name = headers.Filename
+	image.Location = image.ID + filepath.Ext(image.Name)
+
+	// Open a file at the target location
+	savedFile, err := os.Create("./data/images/" + image.Location)
+	if err != nil {
+		return err
+	}
+
+	defer savedFile.Close()
+
+	// Copy the uploaded file to the target location
+	size, err := io.Copy(savedFile, file)
+	if err != nil {
+		return err
+	}
+	image.Size = size
+
+	// Save the image to the database
+	return globalImageStore.Save(image)
+}
